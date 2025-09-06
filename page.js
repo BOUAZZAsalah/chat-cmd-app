8aa5c761cd540ddf1aec70f7b86837aa4bf79571
Discord <bouazzasalah120120@gmail.com>
2025-09-06 10:45:50 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,14 @@
+export default function ChatPage() {
+  return (
+    <div>
+      <h1>غرف الدردشة</h1>
+      <ul>
+        <li>General</li>
+        <li>1 ere</li>
+        <li>2 ème</li>
+        <li>3 ème</li>
+        <li>Application</li>
+      </ul>
+    </div>
+  )
+}
