cf56bcb861bd33054b25a16b3cf27895b24e9721
Discord <bouazzasalah120120@gmail.com>
2025-09-06 10:51:18 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Chat CMD App</h1>
+      <Link href="/chat">ادخل إلى الدردشة</Link>
+    </div>
+  )
+}
